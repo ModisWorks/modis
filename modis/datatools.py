2d35a0ae3fc589848051d3f86573e5814c387513
Joel Launder <jalaunder@gmail.com>
2017-11-05 00:13:01 +1100

    data.json sorting
    Added sorting by type, then key, for data.json

--- modis/datatools.py
+++ modis/datatools.py
@@ -2,6 +2,7 @@
 
 import json as _json
 import os as _os
+from collections import OrderedDict
 
 _dir = _os.path.dirname(_os.path.realpath(__file__))
 _datafile = "{}/../data.json".format(_dir)
@@ -37,5 +38,45 @@ def write_data(data: dict):
     Args:
         data (dict): The updated data dictionary for Modis
     """
+
+    sorted_dict = sort_recursive(data)
+
     with open(_datafile, 'w') as file:
-        _json.dump(data, file, sort_keys=True, indent=2)
+        _json.dump(sorted_dict, file, indent=2)
+
+
+def sort_recursive(data: dict):
+    """
+    Recursively sorts all elements in a dictionary
+
+    Args:
+        data (dict): The dictionary to sort
+
+    Returns:
+        sorted_dict (OrderedDict): The sorted data dict
+    """
+
+    newdict = {}
+
+    for i in data.items():
+        if type(i[1]) is dict:
+            newdict[i[0]] = sort_recursive(i[1])
+        else:
+            newdict[i[0]] = i[1]
+
+    return OrderedDict(sorted(newdict.items(), key=lambda item: (compare_type(type(item[1])), item[0])))
+
+def compare_type(t):
+    """
+    Gives the order of the type for the dictionary
+
+    Args:
+        t (type): The type to compare
+
+    Returns:
+        1 for (dict, OrderedDict), 0 otherwise
+    """
+    if t in [dict, OrderedDict]:
+        return 1
+
+    return 0
