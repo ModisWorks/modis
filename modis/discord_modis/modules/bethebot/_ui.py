f10d06d94b90b21d275797477a9438f9f06d7db5
Joel Launder <jalaunder@gmail.com>
2017-11-08 16:38:24 +1100

    Fixed line endings

--- modis/discord_modis/modules/bethebot/_ui.py
+++ modis/discord_modis/modules/bethebot/_ui.py
@@ -28,18 +28,6 @@ class ModuleUIFrame(ttk.Frame):
         chat = ChatFrame(self)
         chat.grid(column=0, row=0, sticky="W E N S")
 
-        # Help frame
-        help_frame = ttk.LabelFrame(self, padding=8, text="Help")
-        help_frame.grid(row=1, column=0, sticky="W E N S")
-        help_frame.columnconfigure(0, weight=1)
-        help_frame.rowconfigure(0, weight=1)
-        # Find the help path
-        _dir = os.path.realpath(
-            os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        help_path = "{}/{}".format(_dir, "_help.json")
-        # Load the text
-        helptools.add_help_text(help_frame, help_path)
-
 
 class ChatFrame(ttk.LabelFrame):
     """The chat frame for the BeTheBot module"""
