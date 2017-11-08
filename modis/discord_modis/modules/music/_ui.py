3a427858d46a14864ff9a07b228efac5d32b3590
Joel Launder <jalaunder@gmail.com>
2017-11-08 13:35:46 +1100

    Moved help ui to gui

--- modis/discord_modis/modules/music/_ui.py
+++ modis/discord_modis/modules/music/_ui.py
@@ -1,9 +1,6 @@
 import logging
-import os
 import tkinter as tk
 import tkinter.ttk as ttk
-
-from .._tools import helptools
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +16,7 @@ class ModuleUIFrame(ttk.Frame):
             parent: A tk or ttk object
         """
 
-        super(ModuleUIFrame, self).__init__(parent, padding=8)
+        super(ModuleUIFrame, self).__init__(parent)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
 
@@ -34,23 +31,11 @@ class ModuleUIFrame(ttk.Frame):
         # Add key field
         self.google_api_key = tk.StringVar()
         ttk.Label(api_frame, text="Google API Key").grid(column=0, row=0, sticky="W E N S")
-        ttk.Entry(api_frame, textvariable=self.google_api_key).grid(column=0, row=1, padx=4, pady=4, sticky="W E N S")
+        ttk.Entry(api_frame, textvariable=self.google_api_key).grid(column=0, row=1, padx=0, pady=4, sticky="W E N S")
         ttk.Button(api_frame, command=lambda: self.update_google_api_key(), text="Update API Key").grid(
-            column=1, row=1, padx=4, pady=4, sticky="W E N S")
+            column=1, row=1, padx=0, pady=4, sticky="W E N S")
         if "google_api_key" in data["discord"]["keys"]:
             self.google_api_key.set(data["discord"]["keys"]["google_api_key"])
-
-        # Help frame
-        help_frame = ttk.LabelFrame(self, padding=8, text="Help")
-        help_frame.grid(row=1, column=0, sticky="W E N S")
-        help_frame.columnconfigure(0, weight=1)
-        help_frame.rowconfigure(0, weight=1)
-        # Find the help path
-        _dir = os.path.realpath(
-            os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        help_path = "{}/{}".format(_dir, "_help.json")
-        # Load the text
-        helptools.add_help_text(help_frame, help_path)
 
     def update_google_api_key(self):
         """Updates the Google API key with the text value"""
