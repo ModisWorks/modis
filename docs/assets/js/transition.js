b0711b34e09694bb50642a19ba9195e9e22b6616
Joel Launder <jalaunder@gmail.com>
2017-11-13 16:48:05 +1100

    Better mobile support for tabs

--- docs/assets/js/transition.js
+++ docs/assets/js/transition.js
@@ -27,9 +27,9 @@ $(function () {
 
                 // Scroll to top
                 var $pageTabsPage = $("#PageTabsPage");
-                var h = $pageTabsPage.offset().top + 1;
-                if (window.scrollY > h) {
-                    window.scrollTo(0, h);
+                var t = $pageTabsPage.offset().top + 1;
+                if (window.scrollY > t) {
+                    window.scrollTo(0, t);
                 }
             }
         }
@@ -43,26 +43,25 @@ $(function () {
         }
     });
 
-    var updateTabs = function () {
-        var $pageTabsPage = $("#PageTabsPage");
-        var $pageTabsFixed = $("#PageTabsFixed");
-
-        console.log(window.scrollY);
-        var t = $pageTabsPage.offset().top;
-        if (window.scrollY > t) {
-            if (!$pageTabsPage.hasClass("hidden") || $pageTabsFixed.hasClass("hidden")) {
-                $pageTabsPage.addClass("hidden");
-                $pageTabsFixed.removeClass("hidden");
-            }
-        } else {
-            if ($pageTabsPage.hasClass("hidden") || !$pageTabsFixed.hasClass("hidden")) {
-                $pageTabsPage.removeClass("hidden");
-                $pageTabsFixed.addClass("hidden");
-            }
-        }
-    }
-
     // Update tab bar
     updateTabs();
     window.onscroll = updateTabs;
 });
+
+function updateTabs() {
+    var $pageTabsPage = $("#PageTabsPage");
+    var $pageTabsFixed = $("#PageTabsFixed");
+
+    var t = $pageTabsPage.offset().top + 1;
+    if (window.scrollY > t) {
+        if (!$pageTabsPage.hasClass("hidden") || $pageTabsFixed.hasClass("hidden")) {
+            $pageTabsPage.addClass("hidden");
+            $pageTabsFixed.removeClass("hidden");
+        }
+    } else {
+        if ($pageTabsPage.hasClass("hidden") || !$pageTabsFixed.hasClass("hidden")) {
+            $pageTabsPage.removeClass("hidden");
+            $pageTabsFixed.addClass("hidden");
+        }
+    }
+}
