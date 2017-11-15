7e59a2bac07dd5279f295996e349a407556ca47d
Joel Launder <jalaunder@gmail.com>
2017-11-15 12:02:05 +1100

    Updated server welcome

--- modis/discord_modis/modules/!core/api_core.py
+++ modis/discord_modis/modules/!core/api_core.py
@@ -1,6 +1,8 @@
 import importlib
 import os
 
+import discord
+
 from modis import datatools, logger
 from ..._client import client
 
@@ -15,16 +17,11 @@ async def update_server_data(server):
 
     data = datatools.get_data()
     # Add the server to server data if it doesn't yet exist
+    send_welcome_message = False
     if server.id not in data["discord"]["servers"]:
         logger.debug("Adding new server to serverdata")
         data["discord"]["servers"][server.id] = {"prefix": "!"}
-        # Display a welcome message
-        if server.default_channel:
-            hello_message = "Hello! I'm Modis.\n\n" + \
-                            "The prefix is currently `!`, and can be changed at any time using `!prefix`\n\n" + \
-                            "You can use `!help` to get help commands for all modules, " + \
-                            "or {} me to get the server prefix and help commands.".format(server.me.mention)
-            await client.send_message(server.default_channel, hello_message)
+        send_welcome_message = True
 
     # Make sure all modules are in the server
     _dir = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -46,6 +43,33 @@ async def update_server_data(server):
             logger.exception(e)
 
     datatools.write_data(data)
+
+    # Send a welcome message now
+    if send_welcome_message:
+        default_channel = server.default_channel
+        if not default_channel:
+            for channel in server.channels:
+                if channel.name == "general":
+                    default_channel = channel
+                    break
+        if not default_channel:
+            for channel in server.channels:
+                if "general" in channel.name:
+                    default_channel = channel
+                    break
+        if not default_channel:
+            for channel in server.channels:
+                if channel.type == discord.ChannelType.text:
+                    default_channel = channel
+                    break
+
+        # Display a welcome message
+        if default_channel:
+            hello_message = "Hello! I'm Modis.\n\n" + \
+                            "The prefix is currently `!`, and can be changed at any time using `!prefix`\n\n" + \
+                            "You can use `!help` to get help commands for all modules, " + \
+                            "or {} me to get the server prefix and help commands.".format(server.me.mention)
+            await client.send_message(default_channel, hello_message)
 
 
 def remove_server_data(server_id):
