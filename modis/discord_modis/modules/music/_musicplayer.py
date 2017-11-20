93b8b86777ef0638e78663382f7dcd9ba56f4615
Joel Launder <jalaunder@gmail.com>
2017-11-20 20:31:38 +1100

    Updated timeout

--- modis/discord_modis/modules/music/_musicplayer.py
+++ modis/discord_modis/modules/music/_musicplayer.py
@@ -746,7 +746,8 @@ class MusicPlayer:
                 self.prev_queue.pop(0)
 
             try:
-                bopt_list = ["-reconnect 1", "-reconnect_streamed 1", "-reconnect_delay_max 30"]
+                bopt_list = ["-reconnect 1", "-reconnect_streamed 1", "-reconnect_delay_max 10", "-reconnect_at_eof 10",
+                             "-timeout 10", "-multiple_requests 1"]
                 boptions = " {}".format(' '.join(bopt_list))
                 logger.debug("FFmpeg options: {}".format(boptions))
 
