4c0efc7925e73c66da9a4921fccb7e4267d86736
Infraxion <Infraxion@users.noreply.github.com>
2017-12-12 20:38:51 +1100

    made replies conform to new module style from commands api

--- modis/modules/replies/on_message.py
+++ modis/modules/replies/on_message.py
@@ -9,30 +9,22 @@ async def on_message(message):
         message (discord.Message): Input message
     """
 
-    # Simplify message info
-    server = message.server
-    author = message.author
-    channel = message.channel
-    content = message.content
-
-    # TODO port to new activation
-    # if not data["discord"]["servers"][server.id][_data.modulename]["activated"]:
-    #     return
-
     # Only reply to server messages and don't reply to myself
-    if server is not None and author != channel.server.me:
-        # Retrieve replies from server data
-        normal_replies = data.cache["servers"][server.id]["modules"]["replies"]["normal"]
-        tts_replies = data.cache["servers"][server.id]["modules"]["replies"]["tts"]
+    if message.server is None or message.author == message.channel.server.me:
+        return
 
-        # Check normal replies
-        for r in normal_replies.keys():
-            if r in content.lower().replace(' ', ''):
-                await main.client.send_typing(channel)
-                await main.client.send_message(channel, normal_replies[r])
+    # Retrieve replies from server data
+    normal_replies = data.cache["servers"][message.server.id]["modules"]["replies"]["normal"]
+    tts_replies = data.cache["servers"][message.server.id]["modules"]["replies"]["tts"]
 
-        # Check tts replies
-        for r in tts_replies.keys():
-            if r in content.lower().replace(' ', ''):
-                await main.client.send_typing(channel)
-                await main.client.send_message(channel, tts_replies[r])
+    # Check normal replies
+    for r in normal_replies.keys():
+        if r in message.content.lower().replace(' ', ''):
+            await main.client.send_typing(message.channel)
+            await main.client.send_message(message.channel, normal_replies[r])
+
+    # Check tts replies
+    for r in tts_replies.keys():
+        if r in message.content.lower().replace(' ', ''):
+            await main.client.send_typing(message.channel)
+            await main.client.send_message(message.channel, tts_replies[r], tts=True)
