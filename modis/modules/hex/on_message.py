7a1388ec63ac2d45a7a5583e05ff9d06b7d5f22e
Infraxion <Infraxion@users.noreply.github.com>
2017-12-12 20:18:14 +1100

    ported hex to new commands api

--- modis/modules/hex/on_message.py
+++ modis/modules/hex/on_message.py
@@ -11,59 +11,15 @@ async def on_message(message):
         message (discord.Message): Input message
     """
 
-    # Simplify message info
-    server = message.server
-    author = message.author
-    channel = message.channel
-    content = message.content
+    # TODO make an option to limit hex to only the !hex command instead of reading all messages
 
-    # TODO port to new activation
-    # if not data["discord"]["servers"][server.id][_data.modulename]["activated"]:
-    #     return
+    if message.content.startswith(data.cache["servers"][message.server.id]["prefix"]):
+        return
 
-    # Only reply to server messages and don't reply to myself
-    if server is not None and author != channel.server.me:
-        # Commands section
-        prefix = data.cache["servers"][server.id]["prefix"]
-        if content.startswith(prefix):
-            # Parse message
-            package = content.split(" ")
-            command = package[0][len(prefix):]
-            args = package[1:]
-            arg = ' '.join(args)
-
-            # Commands
-            if command == 'hex':
-                await main.client.send_typing(channel)
-
-                # Parse message
-                hex_strs = api_hexconvert.convert_hex_value(arg)
-                # Create embed UI
-                if len(hex_strs) > 0:
-                    for hex_str in hex_strs:
-                        image_url = convert_hex_to_url(hex_str)
-                        embed = ui_embed.success(channel, image_url, hex_str)
-                        await embed.send()
-                else:
-                    embed = ui_embed.fail_api(channel)
-                    await embed.send()
-        else:
-            # Parse message
-            hex_strs = api_hexconvert.convert_hex_value(content)
-            # Create embed UI
-            if len(hex_strs) > 0:
-                for hex_str in hex_strs:
-                    await main.client.send_typing(channel)
-                    image_url = convert_hex_to_url(hex_str)
-                    embed = ui_embed.success(channel, image_url, hex_str)
-                    await embed.send()
-
-
-def convert_hex_to_url(hex_value):
-    """
-    Converts a hex value to a url for an image of that value
-
-    Returns:
-        url (str): A url referencing an image of the given hex value
-    """
-    return "https://dummyimage.com/350x200.png/{0}/{0}".format(hex_value)
+    hex_strs = api_hexconvert.convert_hex_value(message.content)
+    if len(hex_strs) > 0:
+        for hex_str in hex_strs:
+            await main.client.send_typing(message.channel)
+            image_url = "https://dummyimage.com/350x200.png/{0}/{0}".format(hex_str)
+            embed = ui_embed.success(message.channel, image_url, hex_str)
+            await embed.send()
